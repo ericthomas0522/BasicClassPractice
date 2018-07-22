a059fa4932b646e1f08e464699da366ff48ec58f
Eric Thomas <eric.therock@gmail.com>
2018-07-22 11:49:37 -0500

    Getters for values finished

--- BasicClassPractice/Color.cs
+++ BasicClassPractice/Color.cs
@@ -12,7 +12,8 @@ namespace BasicClassPractice
         private int green;
         private int blue;
         private int alpha;
-
+        
+        //Constructors are below. If you don't specify an alpha value it will default to opaque.
         public Color(int red, int green, int blue, int alpha)
         {
             this.red = red;
@@ -21,12 +22,36 @@ namespace BasicClassPractice
             this.alpha = alpha;
         }
 
-        public Color(int red, int green, int blue,)
+        public Color(int red, int green, int blue)
         {
             this.red = red;
             this.green = green;
             this.blue = blue;
             alpha = 255;
         }
+
+        //Returns the value of a color
+        public int GetRed()
+        {
+            return red;
+        }
+
+        public int GetGreen()
+        {
+            return green;
+        }
+
+        public int GetBlue()
+        {
+            return blue;
+        }
+
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+
+        //sets a new value
+
     }
 }
