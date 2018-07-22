12bb219af531d479e5b152ae21a1bc32a7a3ae23
Eric Thomas <eric.therock@gmail.com>
2018-07-22 11:24:16 -0500

    Basic creating of an instance of a custom class

--- BasicClassPractice/Program.cs
+++ BasicClassPractice/Program.cs
@@ -10,6 +10,14 @@ namespace BasicClassPractice
     {
         static void Main(string[] args)
         {
+            //creating a new book.
+            Book book = new Book("Harry Potter", "J.K. Rowling");
+
+            //Updates the title of the book
+            book.SetTitle("Harry Potter and the Half-Blood Prince");
+
+            //returns the title of the book
+            Console.WriteLine(book.GetTitle());
         }
     }
 }
