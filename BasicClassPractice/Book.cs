aef0e447814792231fa8de1f1640319d3d3a02bc
Eric Thomas <eric.therock@gmail.com>
2018-07-22 11:04:29 -0500

    Start of Basic Class practice

---
+++ BasicClassPractice/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassPractice
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+    }
+}
